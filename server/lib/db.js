c6ee84e8387a7545550465c7146fba03c604c8ca
Kamran Majeed <kamranmajeed515@gmail.com>
2026-02-22 11:29:00 +0500

    Add start script for Railway

--- server/lib/db.js
+++ server/lib/db.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         mongoose.connection.on("connected", () => console.log("Database connected"));
-        await mongoose.connect(`${process.env.MONODB_URI}/chat-app`)
+        await mongoose.connect(`${process.env.MONGO_URI}/chat-app`)
     } catch (error) {
         console.log(error);
     }
